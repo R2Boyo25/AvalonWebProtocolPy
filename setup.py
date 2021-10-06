107df3eaabf14cdf114a44d7bfa292ca85d1111b
R2boyo25 <r2boyo25@gmail.com>
2021-10-06 17:36:04 -0500

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='AWP',
+    packages=find_packages(include=['AWP']),
+    version='0.1',
+    description='AvalonWebProtocol',
+    author='R2Boyo25',
+    license='GPLV3',
+    install_requires=[]
+)
