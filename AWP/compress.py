5854597d00b3d864d7a0ed718dce10d1c2ca6d94
R2boyo25 <r2boyo25@gmail.com>
2021-10-08 22:40:39 -0500

    Remove Compression Tests

--- AWP/compress.py
+++ AWP/compress.py
@@ -1,13 +1,6 @@
 import lz4.frame
 def compress(data):
-    print(data)
-    print(lz4.frame.compress(data.encode()))
     return lz4.frame.compress(data.encode())
 
 def decompress(data):
-    print(data)
-    print(lz4.frame.decompress(data))
     return lz4.frame.decompress(data).decode()
-
-print('Hello World I like spaghetti fries and tomato fries and tomato fries')
-print(lz4.frame.compress('Hello World I like spaghetti fries and tomato fries and tomato fries'.encode()))
